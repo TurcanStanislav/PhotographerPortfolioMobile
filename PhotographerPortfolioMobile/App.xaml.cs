157b938e1d2cbde8d312a63cee9d2c07b8a94215
turcan_stanislav <stanislav.turcan@stefanini.com>
2022-12-28 20:12:53 +0200

    Init

---
+++ PhotographerPortfolioMobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotographerPortfolioMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PhotographerPortfolioMobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotographerPortfolioMobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
